a6b283354f96e63b80bd8680d172d5671252db4a
Zachary Ringwood <zringwood@gmail.com>
2023-01-18 16:59:14 -0500

    Add files via upload
    Changed the algorithm that runs the game.

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-var player = true
+var player = 1
 /* Determines which board is legal to play on.*/
 var board = -1
 /* Tracks the smaller cells */
 var cells = Array(81);
 cells.fill(0);
+/* Tracks the larger boards */
 var board_status = Array(9);
 board_status.fill(0)
-function change_color(button){
+//These are all the winning states on a 3x3 board. 
+const w_states = [[0,0,0,0,0,0,1,1,1], [0,0,0,1,1,1,0,0,0],[1,1,1,0,0,0,0,0,0],[1,0,0,1,0,0,1,0,0], [0,1,0,0,1,0,0,1,0],[0,0,1,0,0,1,0,0,1],[1,0,0,0,1,0,0,0,1], [0,0,1,0,1,0,1,0,0]]
+function update_game(button){
    if(board != -1 && Math.floor(button.id/9) != board){
       return 0; 
    }
 
-   if(player){
+   if(player == 1){
       button.style.backgroundColor = "#ff0000";
       cells[button.id] = 1;
-   } else {
+   } else if(player == 2){
       button.style.backgroundColor = "#0000ff";
       cells[button.id] = 2;
    }
-   player = !player;
-   if(board != -1)
-      board_status[board] = check_board(board)
+   
+   if(board != -1){
+      board_status[board] = check_board(cells.slice(board*9,board*9+9), player);
+   }
    if(board_status[button.id%9] == 0){
       board = button.id%9;
    }else{
       board = -1;
    }
-   var gamestate = check_game()
+   player = player%2 + 1;
+   var gamestate = check_game();
    if(gamestate == 1){
       document.getElementById("title").innerHTML = "Game Over! Player 1 Wins!"
    }else if(gamestate == 2){
@@ -37,38 +42,18 @@ function change_color(button){
    
 }
 /*Checks for a victory on a particular board, returns 0, 1 or 2 
-depending on who won the board, 0 if the game is still being played, 
-and -1 if the board was a draw */
-function check_board(index){
-   var init = index*9
-   /*Check horizontal*/
-   for(i = 0;i<9;i+=3){
-      if(cells[init+i] != 0 && (cells[init+i] == cells[init+i+1] && cells[init+i+1] == cells[init+i+2])){
-         return cells[init+i]
+depending on who won the board, 0 if the game is still being played*/
+function check_board(board, p){
+   for(let i in w_states){
+      let match = true
+      for(let j in w_states[i]){
+         if(w_states[i][j]==1 && board[j] != p){
+            match = false;
+         }
       }
-   }
-   /*Check Vertical*/
-   for(i = 0;i<3;i++){
-      if(cells[init+i] != 0 && (cells[init+i] == cells[init+i+3] && cells[init+i+3] == cells[init+i+6])){
-         return cells[init+i]
+      if(match){
+         return p;
       }
-   }
-   /*Check Diagonal*/
-   if(cells[init] != 0 && (cells[init] == cells[init+4] && cells[init+4] == cells[init+8])){
-      return cells[init]
-   }
-   if(cells[init+2] != 0 && (cells[init+2] == cells[init+4] && cells[init+4] == cells[init+6])){
-      return cells[init+2]
-   }
-   var sum = 0
-   /*Check Draw*/
-   for(i = 0;i<9;i++){
-      if(cells[init+i] != 0){
-         sum++;
-      }
-   }
-   if(sum == 9){
-      return -1
    }
    return 0
 }
